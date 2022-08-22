9bcccb5a52f5e9c04795643be510b2dc520625ee
shahinul1986 <shahinul1986@gmail.com>
2022-08-23 01:56:19 +0600

    calculator, counter add

---
+++ Calculator/index.js
@@ -0,0 +1,9 @@
+let a;
+let b;
+let c;
+document.getElementById("submitButton").onclick = function(){
+    a = document.getElementById("aTextBox").ariaValueMax;
+    a = Number(a);
+
+    b = document.getElementById("bTextBox")
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+let x;
+let y = 5;
+let z = 9;
+let maximum;
+let minimum;
+
+// x= Math.round(x);
+// x= Math.cbrt(x);
+// x= Math.ceil(x);
+// x= Math.pow(x,4);
+// x= Math.sqrt(x);
+x=Math.PI
+
+console.log(x);
+
+maximum = Math.max(x,y,z);
+minimum = Math.min(x,y,z);
+// console.log(maximum);
+// console.log(minimum);
